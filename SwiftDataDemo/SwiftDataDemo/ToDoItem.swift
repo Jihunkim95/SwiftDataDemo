621ac1dea9cbdfd5a62addba2d8bd30bbb102450
Jihunkim95 <darak551@naver.com>
2023-12-28 10:04:46 +0900

    add: SwiftData 모델 생성

--- SwiftDataDemo/SwiftDataDemo/ToDoItem.swift
+++ SwiftDataDemo/SwiftDataDemo/ToDoItem.swift
@@ -6,4 +6,19 @@
 //
 
 import SwiftUI
+import SwiftData
 
+// 앱의 데이터 모델을 생성
+// @Model : SwiftData는 자동으로 데이터 클래스에 대한 지속성을 활성화
+@Model
+final class ToDoItem: Identifiable {
+    var id: UUID
+    var name: String
+    var isComplete: Bool
+    
+    init(id: UUID, name: String, isComplete: Bool) {
+        self.id = id
+        self.name = name
+        self.isComplete = isComplete
+    }
+}
